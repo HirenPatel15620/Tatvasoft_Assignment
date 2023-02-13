8a0c4cf1bd191914d933fe4dda4c9c06a488b37a
HirenPatel15620 <123364607+HirenPatel15620@users.noreply.github.com>
2023-02-13 16:54:47 +0530

    Delete MVC_CI-Platform directory

--- MVC_CI-Platform/CI-PlatformWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CI_PlatformWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
