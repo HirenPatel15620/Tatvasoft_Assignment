7df00281908b02f3ae1f78e96d81e03bcaa09d13
hiren <hirenpatel.ict19@gmail.com>
2023-02-20 18:00:43 +0530

    No mission page

--- MVC_CI-platform/CI-PlatformWeb/Controllers/HomeController.cs
+++ MVC_CI-platform/CI-PlatformWeb/Controllers/HomeController.cs
@@ -4,45 +4,49 @@ using System.Diagnostics;
 
 namespace CI_PlatformWeb.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        public IActionResult Forgot()
-        {
-            return View();
-        }
-        public IActionResult Changepassword()
-        {
-            return View();
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+		public IActionResult Forgot()
+		{
+			return View();
+		}
+		public IActionResult Changepassword()
+		{
+			return View();
 
-        }
-        public IActionResult NewRegister()
-        {
-            return View();
-        }
-        public IActionResult LandingPage()
-        {
-            return View();
-        }
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		}
+		public IActionResult NewRegister()
+		{
+			return View();
+		}
+		public IActionResult LandingPage()
+		{
+			return View();
+		}
+		public IActionResult NoFound()
+		{
+			return View();
+		}
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
