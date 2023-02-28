03bae05b98cfaa8d741588142348e965f861eb51
Hiren <hirenpatel.ict19@gmail.com>
2023-02-28 18:32:49 +0530

    make N-tier layout

---
+++ MVC_CI-Platform/CI-platformWeb/CI-platform.Repository/Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using CI_platform.Entities.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI_platform.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        public List<Admin> GetAdminsList();
+    }
+}
---
+++ MVC_CI-Platform/CI-platformWeb/CI-platform/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,76 @@
+﻿using CI_platform.Entities.DataModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CI_platform.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        private readonly CiPlatformDbContext _db;
+        public UserAuthenticationController(CiPlatformDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Admin user)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Admins.Add(user);
+                _db.SaveChanges();
+            }
+            return View();
+        }
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [Route("Register")]
+        [HttpPost]
+
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Users.Add(user);
+                _db.SaveChanges();
+            }
+            return View();
+
+        }
+        [Route("ResetPassword")]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("ResetPassword")]
+        public IActionResult ResetPassword(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View();
+        }
+        [Route("ForgotPassword")]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("ForgotPassword")]
+        public IActionResult ForgotPassword(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
